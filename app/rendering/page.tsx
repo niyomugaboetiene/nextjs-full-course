f61c13779be049e4605eaae8e68e562ae1e0f09a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-23 15:40:10 +0200

    Learn next basic from zero to hero

--- app/rendering/page.tsx
+++ app/rendering/page.tsx
@@ -1,4 +1,28 @@
-"use client"
+// * CSR
+
+// "use client"
+// import { useState, useEffect } from "react"
+
+// type Post = {
+//    userId: number,
+//    id: number,
+//    title: string,
+//    body: string
+// }
+// export default function CSR() {
+//     const [data, setData] = useState<Post | null>(null);
+
+//     useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/posts/1")
+//         .then(res => res.json()).then(setData)
+//     }, [])
+
+//     return <div>{data?.title}</div>
+// }
+
+
+// * SSR
+
 import { useState, useEffect } from "react"
 
 type Post = {
@@ -11,7 +35,7 @@ export default function CSR() {
     const [data, setData] = useState<Post | null>(null);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts/1")
+        fetch("https://jsonplaceholder.typicode.com/posts/1", { cache: 'no-store'})
         .then(res => res.json()).then(setData)
     }, [])
 
