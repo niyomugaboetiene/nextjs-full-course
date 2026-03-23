5278b695aba05034628046dde16a3b30a082ca81
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-23 15:50:38 +0200

    Learn next basic from zero to hero

--- app/rendering/page.tsx
+++ app/rendering/page.tsx
@@ -1,4 +1,4 @@
-// * CSR
+// * CSR: Happens in browser (like normal React)
 
 // "use client"
 // import { useState, useEffect } from "react"
@@ -21,23 +21,31 @@
 // }
 
 
-// * SSR
+// * SSR: Happens in browser (like normal React)
+// export default async function CSR() {
 
-import { useState, useEffect } from "react"
+   
+//         const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", { cache: 'no-store'});
+//        const data = await res.json();
+  
 
-type Post = {
-   userId: number,
-   id: number,
-   title: string,
-   body: string
-}
-export default function CSR() {
-    const [data, setData] = useState<Post | null>(null);
+//     return <div>{data?.title}</div>
+// }
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts/1", { cache: 'no-store'})
-        .then(res => res.json()).then(setData)
-    }, [])
+//* SSG: age is built once at build time
+// export default async function SSR() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", { cache: 'force-cache'});
+//     const data = await res.json();
+
+//     return <div>{data?.title}</div>
+// }
+
+
+//* ISR(Incremental Static regeneration)
+
+export default async function ISR() {
+    const res = await  fetch("https://jsonplaceholder.typicode.com/posts/1", { next: {revalidate: 5}});
+    const data = await res.json();
 
     return <div>{data?.title}</div>
 }
