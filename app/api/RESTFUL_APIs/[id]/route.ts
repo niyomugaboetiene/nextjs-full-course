196b24eb1d6dfe561ea2e3b3389fd49e8ca27c3a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-24 17:40:15 +0200

    Learn next basic (RESTFUL APIs) in next js

--- app/api/RESTFUL_APIs/[id]/route.ts
+++ app/api/RESTFUL_APIs/[id]/route.ts
@@ -1,10 +1,21 @@
 import connection from "../connection";
 
+
+export async function GET({ params } : { params: { id: number }}) {
+    
+}
+
 export async function UPDATE(request: Request, { params } : { params: {id: number }}) {
     try {
        const { name, password } = await request.json();
        const id = params.id;
 
-    
+       await connection.query(
+        'UPDATE Student SET name = ?, password = ?', [name, password]
+       );
+
+       return Response.json({ message: "Updated successfully" });
+    } catch (err) {
+        return Response.json({Error: err});
     }
 }
