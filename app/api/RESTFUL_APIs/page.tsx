6e92c2d0d4ceeb8e48308c518d806333aec50d49
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-24 14:54:21 +0200

    Learn next basic (RESTFUL APIs) in next js

---
+++ app/api/RESTFUL_APIs/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+    const [name, setName] = useState("");
+    const [passowrd, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    async function HandleAdd() {
+        try {
+           fetch("localhost:3000/api/RESTFUL_APIs", { method: 'POST', body: JSON.stringify({name, passowrd})});
+           setMessage("User Added successfully");
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <label>Name</label>
+                <input type="text"  onChange={(e) => setName(e.target.value)} />
+            </div>
+
+            <div>
+                <label>Password</label>
+                <input type="password"  onChange={(e) => setPassword(e.target.value)} />
+            </div>
+
+            <button onClick={HandleAdd}>Add new one</button>
+        </div>
+    )
+}
