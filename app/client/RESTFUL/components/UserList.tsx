59309276a72a1d19318d0f9acae1ef80c58e597e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-03-24 16:28:42 +0200

    Learn next basic (RESTFUL APIs) in next js

---
+++ app/client/RESTFUL/components/UserList.tsx
@@ -0,0 +1,27 @@
+export default async function UserList () {
+       const res = await fetch("http://localhost:3000/api/RESTFUL_APIs", { method: 'GET', cache: 'no-store' });
+        const data = await res.json();
+
+    return (
+        <div>
+            <table border={2}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Password</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {data.map((user: any) => (
+                          <tr key={user.id}>
+                            <td>{user.name}</td>
+                            <td>{user.password}</td>
+                          </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
